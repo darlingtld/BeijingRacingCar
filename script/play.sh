727c1a1b4c69f825c98f221f92c6e227e38d7ed3
lingda <lingda@splunk.com>
2017-09-19 08:06:41 +0800

    make this app as a single application(not a web application)

--- script/play.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#mkdir -p ~/mongodb_data/gamble_db
-#mongod --dbpath ~/mongodb_data/gamble_db
-base_dir=$(cd "$(dirname "$0")";pwd)
-chromedriver_dir=$base_dir/../src/main/resources/chromedriver
-echo $chromedriver_dir
-java -jar build/libs/BeijingRacingCar-0.0.1-SNAPSHOT.jar $chromedriver_dir
-
-
